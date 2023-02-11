a31ef279dab4ee6bd044bb87770acb710281de95
AbdulazizAFM <s202034680@kfupm.edu.sa>
2023-02-11 20:08:57 +0300

    Added selectionSort implementation

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,41 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Sorter {
+    void selectionSort(int arr[])
+    {
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+ 
+    // Prints the array
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+ 
+    // Driver code to test above
+    public static void main(String args[])
+    {
+        Sorter ob = new Sorter();
+        int arr[] = {64,25,12,22,11};
+        ob.selectionSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
     }
 }
